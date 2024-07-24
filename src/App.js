ec45230596efe707ee3ea1475419c744f1305594
Maria <145348994+MariaBezdudnaya@users.noreply.github.com>
2024-07-24 18:18:15 +0500

    new react-calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [result, setResult] = useState('');
+  const [input, setInput] = useState('');
+  const [history, setHistory] = useState([]);
+  
+  const calculateResult = () => { // Вычислить результат
+     try {
+        const newResult = eval(input);
+        if (newResult === Infinity || isNaN(newResult)) { // Проверка деления на ноль или другие ошибки
+            throw new Error('Division by zero or invalid calculation');
+        }
+        setResult(newResult);
+        setHistory([...history, { input, result: newResult }]);
+      } catch (error) {
+        setResult('Error: ' + error.message);
+      };
+   };
+  
+   const useResult = () => { // Использовать результат
+      setInput(result);
+   };
+   const clearResult = () => { // Очистить инпут и результат
+      setInput('');
+      setResult('');
+   };
+   const clearHistory = () => { // Очистить историю
+      setHistory([]);
+   };
+  
+   return (
+    <div className="calculatorBody">
+      <h3>SIMPLE CALCULATOR</h3>
+      <input className="input" type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+      <div>Result: <span className="result">{result}</span></div>
+      <div className="button-container">
+        <button className="button" onClick={calculateResult}>Calculate</button>
+        <button className="button" onClick={useResult}>Use Result</button>
+        <button className="button" onClick={clearResult}>ClearResult</button>
+      </div>
+      
+      <div>History:</div>
+        <ul className="history">
+          {history.map((entry, index) => (
+            <li key={index}>
+              {entry.input} = {entry.result}
+            </li>
+          ))}
+        </ul>
+      <button className="button" onClick={clearHistory}>Clear History</button>
     </div>
   );
 }
